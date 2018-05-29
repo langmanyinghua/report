7928d23a4ef7d5f7af7a33d8d62bda9007aa31d9
panlaixing <1181299257@qq.com>
2018-05-29 15:44:45 +0800

    修改http

--- utils/http.js
+++ utils/http.js
@@ -1,1 +1,51 @@
 const IP = "https://www.amazingfuture.cn"
+
+
+//*******************  登录，注册 start ******************* //
+
+
+/**
+ * 登陆
+ * username 用户名(手机号)
+ * password 密码
+ */
+function login(username, password) {
+  console.log(" 用户名 : ", username)
+}
+
+
+//*******************  登录，注册 end ******************* //
+
+
+//*******************  工具类 start ******************* //
+
+/**
+ * 文件上传
+ * 
+ * filePath 临时文件地址
+ */
+function upload(filePath) {
+  wx.uploadFile({
+    url: IP + "/manager/assist/upload",
+    filePath: filePath,
+    name: 'file',
+    header: {
+      token: getApp.globalData.token
+    },
+    success(res) {
+      console.log("上传结果 : ", res)
+    }
+  })
+}
+
+//*******************  工具类 end ******************* //
+
+
+
+
+/**
+ * 暴露接口
+ */
+module.exports = {
+  login: login
+}
